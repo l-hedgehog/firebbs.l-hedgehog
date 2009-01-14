88f9e8ed111ca66a4861eaf6b57d28ee92a318b8
Hector <l-hedgehog@126.com>
2009-01-15 00:39:10 +0800

    add copy and license block to all js and css

--- chrome/content/input_behavior.js
+++ chrome/content/input_behavior.js
@@ -1,3 +1,24 @@
+/* ***** BEGIN COPYRIGHT AND LICENSE BLOCK *****
+ *
+ * Copyright © 2007,2008,2009 Milx, Hector Zhao
+ *
+ * This file is part of FireBBS.l-hedgehog.
+ *
+ * FireBBS.l-hedgehog is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * FireBBS.l-hedgehog is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * long with FireBBS.l-hedgehog.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ***** END COPYRIGHT AND LICENSE BLOCK ***** */
+
 var isPreventDefault = false;//for events of keyboard & mouse
 var IME = [];
 
