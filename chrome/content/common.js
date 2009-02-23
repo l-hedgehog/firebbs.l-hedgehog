100c3423a66dd522081ce0218344439cd4206d83
Hector <l-hedgehog@126.com>
2009-02-23 22:19:02 +0800

    just a typo in the license blocks

--- chrome/content/common.js
+++ chrome/content/common.js
@@ -16,7 +16,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * long with FireBBS.l-hedgehog.  If not, see <http://www.gnu.org/licenses/>.
+ * along with FireBBS.l-hedgehog.  If not, see <http://www.gnu.org/licenses/>.
  *
  * ***** END COPYRIGHT AND LICENSE BLOCK ***** */
 
